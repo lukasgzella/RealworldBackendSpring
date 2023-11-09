e4f3275b8f838db8a4c0d9bc303b164f32b1a5e5
lukas.gzella <lukas.gzella@gmail.com>
2023-11-09 22:07:55 +0100

    implement Tag

--- src/main/java/com/gzella/realworld/persistence/entity/Tag.java
+++ src/main/java/com/gzella/realworld/persistence/entity/Tag.java
@@ -3,12 +3,15 @@ package com.gzella.realworld.persistence.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@EqualsAndHashCode
 @Table(name = "tags")
 public class Tag {
 
@@ -17,8 +20,32 @@ public class Tag {
     @Column(name = "tag_id")
     private Long id;
     private String name;
-    @ManyToOne
-    @JoinColumn(name="article_id")
-    private Article article;
+    @Builder.Default
+    @ManyToMany(mappedBy = "tagList")
+    private Set<Article> articles = new HashSet<>();
 
+    public Tag(String name) {
+        this.name = name;
+        this.articles = new HashSet<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id) && Objects.equals(name, tag.name) && Objects.equals(articles, tag.articles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
