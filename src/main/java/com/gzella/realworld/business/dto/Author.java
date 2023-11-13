d97e552c238ee4fe09395094abaeafbf6bc4614c
lukas.gzella <lukas.gzella@gmail.com>
2023-11-13 21:28:15 +0100

    implement Author

--- src/main/java/com/gzella/realworld/business/dto/Author.java
+++ src/main/java/com/gzella/realworld/business/dto/Author.java
@@ -1,5 +1,10 @@
 package com.gzella.realworld.business.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Author {
 
     private String username;
@@ -7,4 +12,20 @@ public class Author {
     private String image;
     private boolean following;
 
+    public Author(String username, String bio, String image, boolean following) {
+        this.username = username;
+        this.bio = bio;
+        this.image = image;
+        this.following = following;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "username='" + username + '\'' +
+                ", bio='" + bio + '\'' +
+                ", image='" + image + '\'' +
+                ", following=" + following +
+                '}';
+    }
 }
